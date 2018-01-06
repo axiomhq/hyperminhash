71fca34610c7b874bfc7211d22c1d6de5cdd8a54
Seif Lotfy <seif.lotfy@gmail.com>
2018-01-06 08:36:55 +0100

    Initial work on hyperminhash sketch

---
+++ hyperminhash.go
@@ -0,0 +1,145 @@
+package hyperminhash
+
+import (
+	"math"
+	bits "math/bits"
+
+	metro "github.com/dgryski/go-metro"
+)
+
+const (
+	p     = 14
+	m     = uint32(1 << p) // 16384
+	max   = 64 - p
+	maxX  = math.MaxUint64 >> max
+	alpha = 0.7213 / (1 + 1.079/float64(m))
+	q     = 6 // the number of bits for the LogLog hash
+	r     = 8 // number of bits for the bbit hash
+	_2q   = 1 << q
+	_2r   = 1 << r
+)
+
+func beta(ez float64) float64 {
+	zl := math.Log(ez + 1)
+	return -0.370393911*ez +
+		0.070471823*zl +
+		0.17393686*math.Pow(zl, 2) +
+		0.16339839*math.Pow(zl, 3) +
+		-0.09237745*math.Pow(zl, 4) +
+		0.03738027*math.Pow(zl, 5) +
+		-0.005384159*math.Pow(zl, 6) +
+		0.00042419*math.Pow(zl, 7)
+}
+
+func regSumAndZeros(registers []uint8) (float64, float64) {
+	var sum, ez float64
+	for _, val := range registers {
+		if val == 0 {
+			ez++
+		}
+		sum += 1 / math.Pow(2, float64(val))
+	}
+	return sum, ez
+}
+
+// Sketch is a sketch for cardinality estimation based on LogLog counting
+type Sketch struct {
+	reg [m]uint8
+	sig [m]uint16
+}
+
+// New returns a Sketch
+func New() *Sketch {
+	return new(Sketch)
+}
+
+// AddHash takes in a "hashed" value (bring your own hashing)
+func (sk *Sketch) AddHash(x, y uint64) {
+	k := x >> uint(max)
+	val := uint8(bits.LeadingZeros64((x<<p)^maxX)) + 1
+	if sk.reg[k] < val {
+		sk.reg[k] = val
+		sk.sig[k] = uint16(y)
+	} else if sk.reg[k] == val && sk.sig[k] > uint16(y) {
+		sk.sig[k] = uint16(y)
+	}
+}
+
+// Add inserts a value into the sketch
+func (sk *Sketch) Add(value []byte) {
+	h1, h2 := metro.Hash128(value, 1337)
+	sk.AddHash(h1, h2)
+}
+
+// Cardinality returns the number of unique elements added to the sketch
+func (sk *Sketch) Cardinality() uint64 {
+	sum, ez := regSumAndZeros(sk.reg[:])
+	m := float64(m)
+	return uint64(alpha * m * (m - ez) / (beta(ez) + sum))
+}
+
+func merge(sk1, sk2 *Sketch) *Sketch {
+	m := *sk1
+	for i := range m.reg {
+		if m.reg[i] < sk2.reg[i] {
+			m.reg[i] = sk2.reg[i]
+			m.sig[i] = sk2.sig[i]
+		} else if m.reg[i] == sk2.reg[i] &&
+			m.sig[i] < sk2.sig[i] {
+			m.reg[i] = sk2.reg[i]
+			m.sig[i] = sk2.sig[i]
+		}
+	}
+	return &m
+}
+
+// Merge other into sk
+func (sk *Sketch) Merge(other *Sketch) {
+	*sk = *(merge(sk, other))
+}
+
+// Similarity return a Jaccard Index similarity estimation
+func (sk *Sketch) Similarity(other *Sketch) float64 {
+	var C, N uint64
+	for i := range sk.reg {
+		if sk.reg[i] == other.reg[i] && sk.sig[i] == other.sig[i] {
+			C++
+		}
+		if sk.reg[i] != 0 && sk.sig[i] != 0 &&
+			other.reg[i] != 0 && other.sig[i] != 0 {
+			N++
+		}
+	}
+	n := sk.Cardinality()
+	m := other.Cardinality()
+	ec := sk.expectedCollision(n, m)
+	return (float64(C-ec) / float64(N))
+}
+
+func (sk *Sketch) expectedCollision(n, m uint64) uint64 {
+	var x, b1, b2 float64
+	for i := 1.0; i <= _2q; i++ {
+		for j := 1.0; j <= _2r; j++ {
+			if i != _2q {
+				den := math.Pow(2, float64(p+r+i))
+				b1 = (_2r + j) / den
+				b2 = (_2r + j + 1) / den
+			} else {
+				den := math.Pow(2, float64(p+r+i-1))
+				b1 = j / den
+				b2 = (j + 1) / den
+			}
+			prx := math.Pow(1-b2, float64(n)) - math.Pow(1-b1, float64(n))
+			pry := math.Pow(1-b2, float64(m)) - math.Pow(1-b1, float64(m))
+			x += (prx * pry)
+		}
+	}
+	return uint64((x * float64(p)) + 0.5)
+}
+
+// Intersection returns number of intersections between sk and other
+func (sk *Sketch) Intersection(other *Sketch) uint64 {
+	sim := sk.Similarity(other)
+	m := merge(sk, other)
+	return uint64((sim*float64(m.Cardinality()) + 0.5))
+}
---
+++ hyperminhash_test.go
@@ -0,0 +1,133 @@
+package hyperminhash
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"strconv"
+	"testing"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func estimateError(got, exp uint64) float64 {
+	var delta uint64
+	if got > exp {
+		delta = got - exp
+	} else {
+		delta = exp - got
+	}
+	return float64(delta) / float64(exp)
+}
+
+func TestZeros(t *testing.T) {
+	registers := [m]uint8{}
+	exp := 0.0
+	for i := range registers {
+		val := uint8(rand.Intn(32))
+		if val == 0 {
+			exp++
+		}
+		registers[i] = val
+	}
+	_, got := regSumAndZeros(registers[:])
+	if got != exp {
+		t.Errorf("expected %.2f, got %.2f", exp, got)
+	}
+}
+
+func RandStringBytesMaskImprSrc(n uint32) string {
+	b := make([]byte, n)
+	for i := uint32(0); i < n; i++ {
+		b[i] = letterBytes[rand.Int()%len(letterBytes)]
+	}
+	return string(b)
+}
+
+func TestCardinality(t *testing.T) {
+	sk := New()
+	step := 10000
+	unique := map[string]bool{}
+
+	for i := 1; len(unique) <= 1000000; i++ {
+		str := RandStringBytesMaskImprSrc(rand.Uint32() % 32)
+		sk.Add([]byte(str))
+		unique[str] = true
+
+		if len(unique)%step == 0 {
+			exact := uint64(len(unique))
+			res := uint64(sk.Cardinality())
+			step *= 10
+
+			ratio := 100 * estimateError(res, exact)
+			if ratio > 2 {
+				t.Errorf("Exact %d, got %d which is %.2f%% error", exact, res, ratio)
+			}
+
+		}
+	}
+}
+
+func TestMerge(t *testing.T) {
+	sk1 := New()
+	sk2 := New()
+
+	unique := map[string]bool{}
+
+	for i := 1; i <= 3500000; i++ {
+		str := RandStringBytesMaskImprSrc(rand.Uint32() % 32)
+		sk1.Add([]byte(str))
+		unique[str] = true
+
+		str = RandStringBytesMaskImprSrc(rand.Uint32() % 32)
+		sk2.Add([]byte(str))
+		unique[str] = true
+	}
+
+	sk1.Merge(sk2)
+	exact := len(unique)
+	res := int(sk1.Cardinality())
+
+	ratio := 100 * math.Abs(float64(res-exact)) / float64(exact)
+	expectedError := 1.04 / math.Sqrt(float64(m))
+
+	if float64(res) < float64(exact)-(float64(exact)*expectedError) || float64(res) > float64(exact)+(float64(exact)*expectedError) {
+		t.Errorf("Exact %d, got %d which is %.2f%% error", exact, res, ratio)
+	}
+
+	sk1.Merge(sk2)
+	exact = res
+	res = int(sk1.Cardinality())
+
+	if float64(res) < float64(exact)-(float64(exact)*expectedError) || float64(res) > float64(exact)+(float64(exact)*expectedError) {
+		t.Errorf("Exact %d, got %d which is %.2f%% error", exact, res, ratio)
+	}
+}
+
+func TestCollision(t *testing.T) {
+	sk1 := &Sketch{}
+	sk2 := &Sketch{}
+	unique := map[string]uint{}
+
+	for i := 0; i < 10000000; i++ {
+		str := strconv.Itoa(i)
+		sk1.Add([]byte(str))
+		unique[str]++
+	}
+
+	for i := 9000000; i < 20000000; i++ {
+		str := strconv.Itoa(i)
+		sk2.Add([]byte(str))
+		unique[str]++
+	}
+
+	col := 0
+	for _, count := range unique {
+		if count > 1 {
+			col++
+		}
+	}
+	fmt.Println(sk1.Cardinality())
+	fmt.Println(sk1.Similarity(sk2))
+	fmt.Println(sk1.Intersection(sk2))
+}
